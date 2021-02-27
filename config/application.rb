ed5ef754dc5678d024566be0b1d43dccc80048ca
Michael Antis <antis.michael@gmail.com>
2021-02-27 12:32:17 -0500

    setup devise and its config initializers, generate PostsController

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,31 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+
+# This list is here as documentation only - it's not used
+omitted = %w(
+  action_cable/engine
+  action_mailbox/engine
+  action_text/engine
+)
+
+# Only the frameworks in Rails that do not pollute our routes
+%w(
+  active_storage/engine
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  rails/test_unit/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
